83e4fd460e60b8b1d94e37e79814c4d7e24d3d08
sauravrijal10 <sauravrijal1011@gmail.com>
2024-05-21 15:14:03 +0545

    Feat/Sales implementation

--- custom/Ecommerce_POS/__manifest__.py
+++ custom/Ecommerce_POS/__manifest__.py
@@ -1,10 +1,11 @@
 {
     'name':'Ecommerce_POS',
-    'depends':['base','product'],
+    'depends':['base','product','point_of_sale'],
     'data':[
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/user.xml',
         'views/product.xml',
+        'views/dashboard.xml',
     ]
 }
