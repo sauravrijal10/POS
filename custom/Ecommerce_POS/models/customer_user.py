fdd72a0356376dc554bc6909508a37f978c1138d
Mohitbasnet <mohitbasnet80@gmail.com>
2024-05-21 11:38:24 +0545

    added

--- custom/Ecommerce_POS/models/customer_user.py
+++ custom/Ecommerce_POS/models/customer_user.py
@@ -2,7 +2,10 @@ from odoo import models, fields, api, _
 
 class User(models.Model):
     _name = 'custom.user'
-    _inherit = 'res.partner'
+    _inherits = {'res.partner':'partner_id'}
 
     username = fields.Char(string='Username', required=True, tracking=True)
     password = fields.Char(string='Password', required=True)
+
+    channel_ids = fields.Many2many('mail.channel','mail_channel_profile_partner','partner_id','channel_id')
+
