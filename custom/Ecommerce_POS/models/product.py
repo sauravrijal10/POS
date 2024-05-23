d450b56070968daa1e53d39b5df713b4cb65e686
Jivika <khadkajivika45@gmail.com>
2024-05-23 13:25:59 +0545

    Minor change

--- custom/Ecommerce_POS/models/product.py
+++ custom/Ecommerce_POS/models/product.py
@@ -7,10 +7,6 @@ class CustomProduct(models.Model):
 
     product_template_id = fields.Many2one('product.template', required=True, ondelete='cascade', string='Product Template')
 
-    @api.model
-    def action_open_label_layout(self):
-        # Your code for the action here
-        pass
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -18,25 +14,3 @@ class ProductTemplate(models.Model):
     custom_product_id = fields.Char(string='Custom Product ID')
 
 
-""" class CustomProduct(models.Model):
-    _name = 'custom.product'
-
-    _inherits = {'product.template': 'product_template_id'}
-
-    custom_product_id = fields.Char(string='Custom Product ID')
-    product_template_id = fields.Many2one('product.template', required=True, ondelete='cascade', string='Product Template')
-
-    @api.model
-    def action_open_label_layout(self):
-        # Your code for the action here
-        pass
-    @api.model
-    def custom_action(self):
-        # Add your action logic here
-        pass """
-
-""" def fields_get(self, fields=None):
-        result = super(CustomProduct, self).fields_get(fields=fields)
-        if 'default_code' in result:
-            result['default_code']['invisible'] = True
-        return result """
